3a1b8fd7cbbc080728f5ea10d41b24107c5c688b
sarabuffalo <sep.14094@yahoo.com>
2018-05-07 11:12:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,36 +4,3 @@ function addElementToBeginningOfArray(a, e) {
 var try = ["element"] ["array,"...try]
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
-array.unshift(element);
-return array;
-}
-
-function addElementToEndOfArray(array, element) {
-  return [...array, element]
-}
-
-
-function destructivelyAddElementToEndOfArray(array, element) {
-array.push(element)
-}
-
-function accessElementInArray(array, index) {
-return array[index]
-
-function destructivelyRemoveElementFromBeginningOfArray(array) {
-array.shift()
-}
-
-function removeElementFromBeginningOfArray(array) {
-return array.slice(array)
-}
-
-function destructivelyRemoveElementFromEndOfArray(array) {
-array.pop()
-return array
-}
-
-function removeElementFromEndOfArray(array) {
-return array.slice(0, array.length -1)
-}
